11f3271d0618e4a7028dc4d235215df81db7b185
Leomar F. Osorio <osorioleomar@gmail.com>
2016-10-02 16:50:55 +0800

    added tinyMCE for creating new FAQ

--- application/views/home.php
+++ application/views/home.php
@@ -163,8 +163,10 @@
 </div>
 
     <script src="assets/js/jquery.min.js" type="text/javascript"></script>
-   <!-- <script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script> -->
-   <script src="assets/js/custom.js" type="text/javascript"></script>
+    <!-- <script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script> -->
+    <script src="assets/js/custom.js" type="text/javascript"></script>
+    <script src="assets/js/tinymce/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
     <script type="text/javascript">
 
         /*
