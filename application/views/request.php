ff2e68529d1c4bcaf76d838ab0974da039db6ed6
Leomar F. Osorio <osorioleomar@gmail.com>
2016-10-10 21:45:37 +0800

    started the user request page

---
+++ application/views/request.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Download Request</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div><h1>My Download Request</h1></div>		
+	</div>
+</body>
+</html>
